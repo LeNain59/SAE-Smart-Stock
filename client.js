65f477bb8f40efea46406ae8eef22905dcb6c57b
LeNain59 <gustave.frgt@gmail.com>
2026-02-19 12:43:18 +0100

    Create client.js

---
+++ client.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://zlbidvslsnoqkpkjtyhq.supabase.co",
+    "sb_publishable_6ALVCt3xYSzwYqV3n3GVQA_9c-o0g6s"
+)
+
