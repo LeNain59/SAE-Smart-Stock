4cdda25edd0d6522656a6eda8ed643d66d141f79
LeNain59 <gustave.frgt@gmail.com>
2026-03-05 08:45:56 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // client.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "sb_publishable_6ALVCt3xYSzwYqV3n3GVQA_9c-o0g6s"  // url de supabase
-const supabaseKey = "sb_secret_zJJ7hyLQ8xkNOYoCT1wmXg_xJXKGeXm"  // clef de supabase
+const supabaseUrl = "https://zlbidvslsnoqkpkjtyhq.supabase.co"  // url de supabase
+const supabaseKey = "sb_publishable_6ALVCt3xYSzwYqV3n3GVQA_9c-o0g6s"  // clef de supabase
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
